1c2afcf8b600b7aee3602756bef861878349e3af
JeanStkng <jeancast.jg@gmail.com>
2021-09-13 10:13:30 -0500

    bootstrap and html added

---
+++ src/app/contador/hijo/hijo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styles: [
+  ]
+})
+export class HijoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
